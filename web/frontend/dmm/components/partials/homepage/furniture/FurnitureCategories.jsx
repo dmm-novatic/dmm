ec0f8f917073c15aff65529e2306482b1ee2488f
Harry <nguyenhungbk92@gmail.com>
2023-03-13 12:01:31 +0700

    init DMM version 0.1

---
+++ web/frontend/dmm/components/partials/homepage/furniture/FurnitureCategories.jsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import Link from 'next/link';
+
+const FurnitureCategories = () => {
+    return (
+        <div className="ps-home-categories ps-section--furniture">
+            <div className="container">
+                <div className="ps-section__header">
+                    <h3>Shop by categories</h3>
+                </div>
+                <div className="ps-section__content">
+                    <div className="row">
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/1.png"
+                                    alt="dmm"
+                                />
+                                <p>Sofas</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/2.png"
+                                    alt="dmm"
+                                />
+                                <p>Chairs</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/3.png"
+                                    alt="dmm"
+                                />
+                                <p>Tables</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/4.png"
+                                    alt="dmm"
+                                />
+                                <p>TV Boards</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/5.png"
+                                    alt="dmm"
+                                />
+                                <p>Storages</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/6.png"
+                                    alt="dmm"
+                                />
+                                <p>Rugs</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/7.png"
+                                    alt="dmm"
+                                />
+                                <p>Lamp &amp; Lighting</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/8.png"
+                                    alt="dmm"
+                                />
+                                <p>Furnishings</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/9.png"
+                                    alt="dmm"
+                                />
+                                <p>Ottomans</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/10.png"
+                                    alt="dmm"
+                                />
+                                <p>Shelfs</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/11.png"
+                                    alt="dmm"
+                                />
+                                <p>Kid Furnitures</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                            <div className="ps-block--category">
+                                <Link href="/shop">
+                                    <a></a>
+                                </Link>
+                                <img
+                                    src="/static/img/categories/furniture/12.png"
+                                    alt="dmm"
+                                />
+                                <p>Kitchen</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FurnitureCategories;
